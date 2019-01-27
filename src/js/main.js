44b6150f53824a8940e0bed66ac2672cf9ba1f30
Shemet <vitaliy.shemet@it-devgroup.com>
2019-01-27 17:13:36 +0200

    select

--- src/js/main.js
+++ src/js/main.js
@@ -1,2 +1,15 @@
+$(window).on('load', function () {
+  $('.select__head').on('click', function () {
+    $(this).siblings('.select__drop').toggleClass('open');
+    $(this).toggleClass('open');
+  });
+});
 
-
+$(document).click(function () {
+  var selectHead = $('.select__head');
+  var selectDrop = $('.select__drop');
+  if (!$(event.target).closest('.select').length) {
+    selectHead.removeClass('open');
+    selectDrop.removeClass('open');
+  }
+});
